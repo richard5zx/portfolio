354d8a6168c67d6803a96c9d8f49b87ba6fd38aa
richard5zx <richardchao2009@gmail.com>
2024-09-18 18:58:24 +0800

    tailwind  config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 tailwind.config = {
     theme: {
-        gridTemplateColums: {
-            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        extend: {
+            gridTemplateColums: {
+                'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            fontFamily: {
+                Outfit: ["Outfit", "sans-serif"],
+                Ovo: ["Ovo", "serif"]
+            },
+            animation: {
+                spin_slow: 'spin 6s linear infinite'
+            },
+            colors: {
+                lightHover: '#fcf4ff',
+                darkHover: '2a004a',
+            }
         },
-        fontFamily: {
-            Outfit: ["Outfit", "sans-serif"],
-            Ovo: ["Ovo", "serif"]
-        },
-        animation: {
-            spin_slow: 'spin 6s linear infinite'
-        }
-    }    
+    }
 }
