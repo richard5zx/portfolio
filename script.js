2f12f732f233e93d477dc78d4f62c371d03dfa0a
richard5zx <richardchao2009@gmail.com>
2024-09-19 14:23:38 +0800

    dark mode implementation

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ window.addEventListener('scroll', ()=> {
         navLinks.classList.add('bg-white', 'shadow-sm','bg-opacity-50');
     }
 })
+
+// ------ Light and DARK Mode ------
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
+
+function toggleTheme() {
+    document.documentElement.classList.toggle('dark')
+    if(document.documentElement.classList.contains('dark')) {
+        localStorage.theme = 'dark';
+    } else {
+        localStorage.theme = 'light';
+    }
+}
