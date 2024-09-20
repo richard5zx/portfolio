07266d8ca2f2fd18fc50c64833aa4ee04bdad61e
richard5zx <richardchao2009@gmail.com>
2024-09-20 18:37:54 +0800

    dark mode done

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function closeMenu() {
 
 window.addEventListener('scroll', ()=> {
     if(scrollY > 50) {
-        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm')
-        navLinks.classList.remove('bg-white', 'shadow-sm','bg-opacity-50');
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20')
+        navLinks.classList.remove('bg-white', 'shadow-sm','bg-opacity-50', 'dark:border-white/50', 'dark:bg-transparent');
     } else {
-        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm')
-        navLinks.classList.add('bg-white', 'shadow-sm','bg-opacity-50');
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20')
+        navLinks.classList.add('bg-white', 'shadow-sm','bg-opacity-50', 'dark:border-white/50', 'dark:bg-transparent');
     }
 })
 
